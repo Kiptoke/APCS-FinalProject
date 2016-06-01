10bcf81989226f53f598b0c6a922879aded57f2d
Kiptoke <kiptoke@gmail.com>
2016-05-31 20:35:41 -0400

    FINAL COMMIT - Documentation
    Documentation added. This should be the final commit for this file.

--- APCS-GuessTheNumber.py
+++ APCS-GuessTheNumber.py
@@ -1,33 +1,32 @@
 #Andrew Zhou
 #Guess the Number Program - Final Project
 
-
-# TODO: finish restart error
-
+# ------ Importing Modules and Libraries ------
 
 import random
 import sys
 
-# --- Class Definition ---
+# ------ Class Definition -------
 
 class RandomNumberGame:
     'The class that runs the game'
-    rounds = 1
 
-    def __init__(self):
-        self.randomNum = 0
-        self.guessNum = 0
-        self.guesses = 0
-        RandomNumberGame.rounds += 1
-
-    def generateNum(self):
-        self.randomNum = random.randint(0, 10)
+    # Initializing Method
+    # Creates variables
     
-    def getUserInput(self):
-        self.guessNum = int(input("Guess a number between 1 and 10: "))
+    def __init__(self):
+        self.randomNum = 0      # Variable for Random Integer
+        self.guessNum = 0       # Variable for user input
+        self.guesses = 0        # Varaible for number of guesses
+
+    def generateNum(self):                                                  # Generates a Random integer between 1 and 10, inclusive
+        self.randomNum = random.randint(1, 10)
+    
+    def getUserInput(self):                                                 # Gets user input - an integer
+        self.guessNum = int(input("Guess a number between 1 and 10: ")) 
         self.compareNumbers()
 
-    def restart(self):
+    def restart(self):                                                      # Restart Method - asks if user wants to play another game
         print("Do you want to play another round?")
         self.restart = input()
         if (self.restart == "yes") or (self.restart == "Yes"):
@@ -36,12 +35,12 @@ class RandomNumberGame:
         else:
             sys.exit()
 
-    def checkLoss(self):
+    def checkLoss(self):                                                    # Checks if player has lost the game
         if (self.guesses >= 3):
             print("You lose!")
             self.restart()
 
-    def compareNumbers(self):
+    def compareNumbers(self):                                               # Compares the User Input to the Random Integer
         if (self.randomNum == self.guessNum):
             print("You won!")
             self.restart()
@@ -56,13 +55,14 @@ class RandomNumberGame:
             self.checkLoss()
             self.getUserInput()
 
-    def runGame(self):
+    def runGame(self):                                                      # Game Method - runs the game
         self.generateNum()
         self.getUserInput()
 
-# --- Main Code ---
+# ------ Main Code ------
 
-print("\t------GUESS THE NUMBER------")
+print("\t------GUESS THE NUMBER------")     # Create Title
 
-test = RandomNumberGame()
-test.runGame()
+test = RandomNumberGame()                   # Creates Game Object
+test.runGame()                              # Runs Game
+
